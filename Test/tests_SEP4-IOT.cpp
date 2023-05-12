97655a4af9dbb042bb22074aa1837439a706ee9a
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2023-05-12 13:24:01 +0200

    test of SensorData
    testet sensorData

--- Test/tests_SEP4-IOT.cpp
+++ Test/tests_SEP4-IOT.cpp
@@ -31,7 +31,101 @@ protected:
 	{}
 };
 
-// Test that the task is created correct
-TEST_F(Test_fixture, Test_createTask1){
-    ASSERT_EQ(1,1);
+TEST_F(Test_fixture, Test_sensorDataMeasure)
+{
+	// Create sensorData struct
+	sensorData_t data = sensorData_init();
+
+	// Set up fake values for hih8120_measure and hih8120_getTemperature_x10
+	hih8120_measure_fake.return_val = HIH8120_OK;
+	hih8120_getTemperature_x10_fake.return_val = 250;
+
+	// Call sensorData_measure
+	sensorData_measure(data);
+
+	// Check that the values have been updated correctly
+	ASSERT_EQ(data->totalTemperature, 250);
+	ASSERT_EQ(data->totalHumidity, 0);
+	ASSERT_EQ(data->totalCarbondioxide, 0);
+	ASSERT_EQ(data->counter, 1);
+
+	// Clean up
+	sensorData_destroy(data);
+}
+
+
+TEST_F(Test_fixture, Test_sensorDataTemperatureAverage)
+{
+	// Create sensorData struct and set initial values
+	sensorData_t data = sensorData_init();
+	data->totalTemperature = 5000;
+	data->counter = 5;
+
+	// Call sensorData_getTemperatureAverage
+	uint16_t average = sensorData_getTemperatureAverage(data);
+
+	// Check that the average is calculated correctly
+	ASSERT_EQ(average, 1000-40);
+
+	// Clean up
+	sensorData_destroy(data);
+}
+
+
+TEST_F(Test_fixture, Test_sensorDataReset)
+{
+	// Create sensorData struct and set initial values
+	sensorData_t data = sensorData_init();
+	data->totalTemperature = 100;
+	data->totalHumidity = 200;
+	data->totalCarbondioxide = 300;
+	data->counter = 4;
+
+	// Call sensorData_reset
+	sensorData_reset(data);
+
+	// Check that the values have been reset to zero
+	ASSERT_EQ(data->totalTemperature, 0);
+	ASSERT_EQ(data->totalHumidity, 0);
+	ASSERT_EQ(data->totalCarbondioxide, 0);
+	ASSERT_EQ(data->counter, 0);
+
+	// Clean up
+	sensorData_destroy(data);
+}
+
+
+TEST_F(Test_fixture, Test_sensorDataHumidityAverage)
+{
+	// Create sensorData struct and set initial values
+	sensorData_t data = sensorData_init();
+	data->totalHumidity = 300;
+	data->counter = 3;
+
+	// Call sensorData_getHumidityAverage
+	uint16_t average = sensorData_getHumidityAverage(data);
+
+	// Check that the average is calculated correctly
+	ASSERT_EQ(average, 100);
+
+	// Clean up
+	sensorData_destroy(data);
+}
+
+
+TEST_F(Test_fixture, Test_sensorDataCarbondioxideAverage)
+{
+	// Create sensorData struct and set initial values
+	sensorData_t data = sensorData_init();
+	data->totalCarbondioxide = 150;
+	data->counter = 2;
+
+	// Call sensorData_getCarbondioxideAverage
+	uint16_t average = sensorData_getCarbondioxideAverage(data);
+
+	// Check that the average is calculated correctly
+	ASSERT_EQ(average, 75);
+
+	// Clean up
+	sensorData_destroy(data);
 }
