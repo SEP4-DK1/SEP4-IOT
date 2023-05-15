a6881b1193ba7133d9fc4d5a6f156dce6f29d99c
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2023-05-15 12:32:37 +0200

    diagram : added the new  LoRaWAN util file to diagram
    - moved the order of methods in TemperatureTransmit.h

--- SEP4-IOT/Tasks/TemperatureTransmit.h
+++ SEP4-IOT/Tasks/TemperatureTransmit.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <ATMEGA_FreeRTOS.h>
 
-inline void temperatureTransmit_taskInit(void* pvParameters);
-inline void temperatureTransmit_taskRun();
-
 void temperatureTransmit_createTask(UBaseType_t taskPriority, void* pvParameters);
 void temperatureTransmit_task(void *pvParameters);
+
+inline void temperatureTransmit_taskInit(void* pvParameters);
+inline void temperatureTransmit_taskRun();
