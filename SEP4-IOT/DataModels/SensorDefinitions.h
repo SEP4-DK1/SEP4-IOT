a30bf9e54f70281becd03ee4f0b2d20e7aee55eb
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2023-05-17 09:45:21 +0200

    Added SensorDefinitions

--- SEP4-IOT/DataModels/SensorDefinitions.h
+++ SEP4-IOT/DataModels/SensorDefinitions.h
@@ -1,6 +1,6 @@
 #define TEMPERATURE_OFFSET 200
 // TEMPERATURE_OFFSET is used to offset the temperature
-// A temperature of -20Â°C is equivalent to 0 in our code.
+// A temperature of -20°C is equivalent to 0 in our code.
 // This is done to avoid sending negative numbers
 // It also allows the temperature to only use 10 bits, which is practical for LoRaWAN
-// Temperature range 0-1023 (-20Â° to 82.3Â°)
+// Temperature range 0-1023 (-20° to 82.3°)
