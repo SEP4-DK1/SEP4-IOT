25aeadf3923c3ddc5dcd745e783ec44521be159e
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2023-05-18 12:38:20 +0200

    Update tests_SEP4-IOT.cpp
    updated the test after reworking the code

--- Test/tests_SEP4-IOT.cpp
+++ Test/tests_SEP4-IOT.cpp
@@ -8,12 +8,15 @@ extern "C"
 {
 	#include "SensorData.h"
 	#include "hih8120.h"
+	#include "mh_z19.h"
 }
 
 FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_measure);
 FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_wakeup);
 FAKE_VALUE_FUNC(int16_t, hih8120_getTemperature_x10);
-FAKE_VALUE_FUNC(uint16_t, hih8120_getHumidityPercent_x10);
+FAKE_VALUE_FUNC(uint16_t, hih8120_getHumidityPercent_x10)
+FAKE_VALUE_FUNC(mh_z19_returnCode_t, mh_z19_takeMeassuring);
+FAKE_VALUE_FUNC(mh_z19_returnCode_t, mh_z19_getCo2Ppm, uint16_t*);
 
 // Create Test fixture and Reset all Mocks before each test
 class Test_fixture : public ::testing::Test
@@ -32,29 +35,6 @@ protected:
 	{}
 };
 
-TEST_F(Test_fixture, Test_sensorDataMeasureHumidity)
-{
-	// Create sensorData struct
-	sensorData_t data = sensorData_init();
-
-	// Set up fake values for hih8120_measure and hih8120_getTemperature_x10
-	hih8120_measure_fake.return_val = HIH8120_OK;
-	hih8120_getHumidityPercent_x10_fake.return_val = 50;
-
-	// Call sensorData_measure
-	sensorData_measure(data);
-
-	// Check that the values have been updated correctly
-	ASSERT_EQ(data->totalTemperature, 0);
-	ASSERT_EQ(data->totalHumidity, 50);
-	ASSERT_EQ(data->totalCarbondioxide, 0);
-	ASSERT_EQ(data->counter, 1);
-
-	// Clean up
-	sensorData_destroy(data);
-}
-
-
 TEST_F(Test_fixture, Test_sensorDataMeasureTemperature)
 {
 	// Create sensorData struct
@@ -64,19 +44,60 @@ TEST_F(Test_fixture, Test_sensorDataMeasureTemperature)
 	hih8120_measure_fake.return_val = HIH8120_OK;
 	hih8120_getTemperature_x10_fake.return_val = 250;
 
+
 	// Call sensorData_measure
 	sensorData_measure(data);
 
 	// Check that the values have been updated correctly
 	ASSERT_EQ(data->totalTemperature, 250);
-	ASSERT_EQ(data->totalHumidity, 0);
-	ASSERT_EQ(data->totalCarbondioxide, 0);
 	ASSERT_EQ(data->counter, 1);
 
 	// Clean up
 	sensorData_destroy(data);
 }
 
+TEST_F(Test_fixture, Test_sensorDataMeasureHumidity)
+{
+    // Create sensorData struct
+    sensorData_t data = sensorData_init();
+
+    // Set up fake values for hih8120_measure and hih8120_getHumidityPercent_x10
+    hih8120_measure_fake.return_val = HIH8120_OK;
+    hih8120_getHumidityPercent_x10_fake.return_val = 500; 
+
+    // Call sensorData_measure
+    sensorData_measure(data);
+    // Check that the values have been updated correctly
+    ASSERT_EQ(data->totalHumidity, 50);
+    ASSERT_EQ(data->counter, 1);
+
+    // Clean up
+    sensorData_destroy(data);
+}
+
+TEST_F(Test_fixture, Test_sensorDataMeasureCo2)
+{
+    // Create sensorData struct
+    sensorData_t data = sensorData_init();
+
+    // Set up fake values
+    mh_z19_takeMeassuring_fake.return_val = MHZ19_OK;
+    
+	mh_z19_getCo2Ppm_fake.custom_fake = [](uint16_t *ppm) {
+        *ppm = 100;
+        return MHZ19_OK;
+    };
+
+    // Call sensorData_measure
+    sensorData_measure(data);
+
+    // Check that the values have been updated correctly
+    ASSERT_EQ(data->totalCarbondioxide, 100); 
+    ASSERT_EQ(data->counter, 1);
+
+    // Clean up
+    sensorData_destroy(data);
+}
 
 TEST_F(Test_fixture, Test_sensorDataTemperatureAverage)
 {
@@ -94,7 +115,6 @@ TEST_F(Test_fixture, Test_sensorDataTemperatureAverage)
 	// Clean up
 	sensorData_destroy(data);
 }
-
 
 TEST_F(Test_fixture, Test_sensorDataReset)
 {
@@ -118,7 +138,6 @@ TEST_F(Test_fixture, Test_sensorDataReset)
 	sensorData_destroy(data);
 }
 
-
 TEST_F(Test_fixture, Test_sensorDataHumidityAverage)
 {
 	// Create sensorData struct and set initial values
@@ -135,7 +154,6 @@ TEST_F(Test_fixture, Test_sensorDataHumidityAverage)
 	// Clean up
 	sensorData_destroy(data);
 }
-
 
 TEST_F(Test_fixture, Test_sensorDataCarbondioxideAverage)
 {
