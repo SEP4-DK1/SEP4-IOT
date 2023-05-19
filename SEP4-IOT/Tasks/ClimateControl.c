07cfe680136142944c521f810cc67990037031eb
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2023-05-19 10:03:54 +0200

    added a servos

--- SEP4-IOT/Tasks/ClimateControl.c
+++ SEP4-IOT/Tasks/ClimateControl.c
@@ -2,6 +2,7 @@
 #include <hih8120.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <rc_servo.h>
 
 climateControlParams_t climateControl_createParams(SemaphoreHandle_t mutex, sensorData_t sensorData, breadConfig_t breadConfig) {
 	climateControlParams_t climateControlParams;
@@ -41,41 +42,50 @@ void climateControl_task(void *pvParameters){
 	{
 		xTaskDelayUntil( &xLastWakeTime, xFrequency );
 
-    if (sensorData->latestTemperature = breadConfig->temperature) {
+    if (sensorData->latestTemperature - breadConfig->temperature = 50 || -50 && sensorData->latestHumidity - breadConfig->humidity = 50 || -50) {
       // Turn up heater to 5% as a holding effect
+	  rc_servo_setPosition(0, -95);
     }
 	 else if (sensorData->latestTemperature < breadConfig->temperature - 300) {
       // Turn up heater to 80%
+	  rc_servo_setPosition(0, 80);
     }
 	 else if (sensorData->latestTemperature < breadConfig->temperature - 200) {
       // Turn up heater to 60%
+	  rc_servo_setPosition(0, 10);
     }
     else if (sensorData->latestTemperature < breadConfig->temperature - 100) {
       // Turn up heater to 50%
+	  rc_servo_setPosition(0, 0);
     }
-    else if (sensorData->latestTemperature < breadConfig->temperature - 50) {
+    else if (sensorData->latestTemperature < breadConfig->temperature - 500) {
       // Turn up heater to 10%
+	  rc_servo_setPosition(0, -90);
     }
 
-    if (sensorData->latestTemperature > breadConfig->temperature + 10) {
+    if (sensorData->latestTemperature > breadConfig->temperature + 100) {
       // turn heater to 0% 
+	  rc_servo_setPosition(0, -100);
     }
 
-	if (sensorData->latestTemperature > breadConfig->temperature + 50) {
+	if (sensorData->latestTemperature > breadConfig->temperature + 500) {
       //open ventilation
+	  rc_servo_setPosition(1, 100);
     }
 
-	/*
-	if (sensorData->latestHumidity > breadConfig->humidity + 5) {
+	
+	if (sensorData->latestHumidity > breadConfig->humidity + 50) {
       // open ventilation 
+	  rc_servo_setPosition(1, 100);
     }
 
-	else if (sensorData->latestHumidity = breadConfig->humidity){
-		// Turn up heater to 5% as a holding effect
-	}
-
-	else if (sensorData->latestHumidity < breadConfig->humidity - 5){
+	else if (sensorData->latestHumidity < breadConfig->humidity - 50){
 		// Turn up heater 100% for 3 sec
+		const TickType_t xFrequency = pdMS_TO_TICKS(3000UL); 
+		rc_servo_setPosition(0, 100);
+		xTaskDelayUntil(xFrequency );
+
+		rc_servo_setPosition(0, -95);
 	}
 
 	if (sensorData->latestCo2 =< 2500) {
@@ -83,9 +93,10 @@ void climateControl_task(void *pvParameters){
 	  // https://www.canada.ca/en/health-canada/programs/consultation-residential-indoor-air-quality-guidelines-carbon-dioxide/document.html#:~:text=of%20this%20assessment.-,Figure%201.%20Comparison%20of%20CO2%20concentrations%20in%20Canadian%20homes%20to%20CO2%20concentrations%20associated%20with%20health,-effects
 	  // in this link tells us what happens at the difrent consentrations of Co2 We agreed to keep it safe to the baker opening it. 
 	  // open ventilation 
+	  rc_servo_setPosition(1, 100);
     }
 
-	*/
+	
 
 	}
 }
