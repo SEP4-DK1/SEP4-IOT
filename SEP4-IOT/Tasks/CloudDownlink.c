fe95262d04721fcbceca090cb474d875140ecbb9
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2023-05-23 09:51:07 +0200

    test cloudDownlink : made the cloudDownlink task testable
    * moved handling of pvParameters into the `cloudDownlink_init`
    * * moved the mutex, messagebuffer, og breadconfig
    
    * moved the running par of the code into `cloudDownlink_run`

--- SEP4-IOT/Tasks/CloudDownlink.c
+++ SEP4-IOT/Tasks/CloudDownlink.c
@@ -37,35 +37,46 @@ void cloudDownlink_createTask(UBaseType_t taskPriority, void* pvParameters) {
 		,  NULL );
 }
 
+SemaphoreHandle_t mutex;
+MessageBufferHandle_t downLinkMessageBufferHandle;
+breadConfig_t breadConfig;
+
+inline void cloudDownlink_init(void* pvParameters) {
+	cloudDownlinkParams_t params = (cloudDownlinkParams_t) pvParameters;
+	mutex = params->mutex;
+	downLinkMessageBufferHandle = params->messageBufferHandle;
+	breadConfig = params->breadConfig;
+	cloudDownlink_destroyParams(params);
+}
+
+inline void cloudDownlink_run() {
+  lora_driver_payload_t downlinkPayload;
+		
+  printf("Listening for downlink...\n");
+  if (xMessageBufferReceive(downLinkMessageBufferHandle, &downlinkPayload, sizeof(lora_driver_payload_t), portMAX_DELAY) == 0) {
+    printf("Downlink failed (payload was empty)\n");
+  }
+  else {
+    printf("DOWN LINK: from port: %d with %d bytes received!\n", downlinkPayload.portNo, downlinkPayload.len); // Just for Debug
+    if (downlinkPayload.len == 3) {
+      uint16_t readTemp = (uint16_t) (((downlinkPayload.bytes[1] & 0b11000000) << 2) + (0b11111111 & downlinkPayload.bytes[0]));
+      uint8_t readHum = (uint8_t) (((downlinkPayload.bytes[2] & 0b10000000) >> 1) + (downlinkPayload.bytes[1] & 0b00111111));
+
+      breadConfig->temperature = readTemp;
+      breadConfig->humidity = readHum;
+      printf("Temperature received: %d\tHumidity received: %d\n", readTemp, readHum);
+    }
+  }
+}
+
 void cloudDownlink_task(void* pvParameters) {
 	TickType_t xLastWakeTime = xTaskGetTickCount();
 	const TickType_t xFrequency = pdMS_TO_TICKS(240000UL); // This delay doesn't really matter as xMessageBufferReceive will pause this task until an uplink message is sent from the temperatureTransmit task
 	
-	cloudDownlinkParams_t params = (cloudDownlinkParams_t) pvParameters;
-	SemaphoreHandle_t mutex = params->mutex;
-	MessageBufferHandle_t downLinkMessageBufferHandle = params->messageBufferHandle;
-	breadConfig_t breadConfig = params->breadConfig;
-	cloudDownlink_destroyParams(params);
+  cloudDownlink_init(pvParameters);
 
 	for(;;) {
 		xTaskDelayUntil( &xLastWakeTime, xFrequency );
-		
-		lora_driver_payload_t downlinkPayload;
-		
-		printf("Listening for downlink...\n");
-		if (xMessageBufferReceive(downLinkMessageBufferHandle, &downlinkPayload, sizeof(lora_driver_payload_t), portMAX_DELAY) == 0) {
-      printf("Downlink failed (payload was empty)\n");
-    }
-    else {
-      printf("DOWN LINK: from port: %d with %d bytes received!\n", downlinkPayload.portNo, downlinkPayload.len); // Just for Debug
-			if (downlinkPayload.len == 3) {
-				uint16_t readTemp = (uint16_t) (((downlinkPayload.bytes[1] & 0b11000000) << 2) + (0b11111111 & downlinkPayload.bytes[0]));
-				uint8_t readHum = (uint8_t) (((downlinkPayload.bytes[2] & 0b10000000) >> 1) + (downlinkPayload.bytes[1] & 0b00111111));
-
-				breadConfig->temperature = readTemp;
-				breadConfig->humidity = readHum;
-				printf("Temperature received: %d\tHumidity received: %d\n", readTemp, readHum);
-			}
-    }
+		cloudDownlink_run();		
 	}
 }
